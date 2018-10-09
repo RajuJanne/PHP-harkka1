e6f342752f83924968458ef19082bd41853c55e4
RajuJanne <janne.rajuvaara@student.samk.fi>
2018-10-09 23:13:33 +0300

    Small beauty fixes

--- index.php
+++ index.php
@@ -1,35 +1,36 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <!-- Tehkää lomake, jossa on kaksi kenttää.
-          Toinen kenttä on käyttäjätunnus ja toinen salasanakenttä.
-          Lomake lähettää tiedot ulkopuoliseen käsittelijään nimeltä kasittelija.php.
-          Kasittelija.php:n tehtävänä on katsoa, että salasana on oikein. Käsittelijä ymmärtää vähintään yhden esiasetetun käyttäjätunnus ja salasana parin.
-          Jos salasana on oikein käsittelijä vastaa viestillä, jossa käsittelijä kertoo kirjautumisen onnistuneen.
-          Tämän viestin alla on kaksi vapaavalintaista linkkiä, jotka näkyy vain kirjautuneelle.
-          Jos salasana tai käyttäjätunnus on väärin, käsittelijä ilmoittaa siitä ja tarjoaa linkin takaisin lomakkeeseen.
-          Käsittelijän tulee kirjautumisen onnistuessa käynnistää sessio. Tutustukaa siis uuteen asiaan $_SESSION.
-          Session pitää muistaa kirjautumisenne ja tarjota kirjautuneelle logout-linkki.
-          Kirjautuneena oleminen tarkoittaa siis sitä, että vaikka päivitätte selaimen tai suljette kyseisen käsittelijävälilehden sekä palatessanne käsittelijä muistaa edelleen, että olette kirjautuneena.
+<head>
+  <meta charset="utf-8">
+  <meta name="author" content="Janne Rajuvaara">
+  <title>PHP pakollinen tehtävä 1</title>
+</head>
+<body>
+  <!-- Tehkää lomake, jossa on kaksi kenttää.
+  Toinen kenttä on käyttäjätunnus ja toinen salasanakenttä.
+  Lomake lähettää tiedot ulkopuoliseen käsittelijään nimeltä kasittelija.php.
+  Kasittelija.php:n tehtävänä on katsoa, että salasana on oikein. Käsittelijä ymmärtää vähintään yhden esiasetetun käyttäjätunnus ja salasana parin.
+  Jos salasana on oikein käsittelijä vastaa viestillä, jossa käsittelijä kertoo kirjautumisen onnistuneen.
+  Tämän viestin alla on kaksi vapaavalintaista linkkiä, jotka näkyy vain kirjautuneelle.
+  Jos salasana tai käyttäjätunnus on väärin, käsittelijä ilmoittaa siitä ja tarjoaa linkin takaisin lomakkeeseen.
+  Käsittelijän tulee kirjautumisen onnistuessa käynnistää sessio. Tutustukaa siis uuteen asiaan $_SESSION.
+  Session pitää muistaa kirjautumisenne ja tarjota kirjautuneelle logout-linkki.
+  Kirjautuneena oleminen tarkoittaa siis sitä, että vaikka päivitätte selaimen tai suljette kyseisen käsittelijävälilehden sekä palatessanne käsittelijä muistaa edelleen, että olette kirjautuneena.
 
-    Onnea! -->
-    <div id="index-login-form-parent">
-      <div id="index-login-form">
-        <form id="login" action="kasittelija.php" method="post">
-          Username: <input type="text" name="username"><br />
-          Password: <input type="password" name="password" autocomplete="off" placeholder="******"><br />
-          <input type="submit" name="submit" value=" Login ">
-          <span><?php if(isset($_SESSION['error'])) echo $_SESSION['error']; ?></span>
-        </form>
-      </div>
+  Onnea! -->
+  <div id="index-login-form-parent">
+    <div id="index-login-form">
+      <form id="login" action="kasittelija.php" method="post">
+        Username: <input type="text" name="username"><br />
+        Password: <input type="password" name="password" autocomplete="off" placeholder="******"><br />
+        <input type="submit" name="submit" value=" Login ">
+        <span><?php if(isset($_SESSION['error'])) echo $_SESSION['error']; ?></span>
+      </form>
     </div>
-	<div id="index-login-postit-note">
-		<p>Username = Timo <br />Password = Soini</p>
-	</div>
-  </body>
+  </div>
+  <div id="index-login-postit-note">
+    <p>Username = Timo <br />Password = Soini</p>
+  </div>
+</body>
 </html>
--- kasittelija.php
+++ kasittelija.php
@@ -1,35 +1,35 @@
-<!--           Kasittelija.php:n tehtävänä on katsoa, että salasana on oikein. Käsittelijä ymmärtää vähintään yhden esiasetetun käyttäjätunnus ja salasana parin.
-          Jos salasana on oikein käsittelijä vastaa viestillä, jossa käsittelijä kertoo kirjautumisen onnistuneen.
-          Tämän viestin alla on kaksi vapaavalintaista linkkiä, jotka näkyy vain kirjautuneelle.
-          Jos salasana tai käyttäjätunnus on väärin, käsittelijä ilmoittaa siitä ja tarjoaa linkin takaisin lomakkeeseen.
-          Käsittelijän tulee kirjautumisen onnistuessa käynnistää sessio. Tutustukaa siis uuteen asiaan $_SESSION.
-          Session pitää muistaa kirjautumisenne ja tarjota kirjautuneelle logout-linkki.
+<!-- Kasittelija.php:n tehtävänä on katsoa, että salasana on oikein. Käsittelijä ymmärtää vähintään yhden esiasetetun käyttäjätunnus ja salasana parin.
+Jos salasana on oikein käsittelijä vastaa viestillä, jossa käsittelijä kertoo kirjautumisen onnistuneen.
+Tämän viestin alla on kaksi vapaavalintaista linkkiä, jotka näkyy vain kirjautuneelle.
+Jos salasana tai käyttäjätunnus on väärin, käsittelijä ilmoittaa siitä ja tarjoaa linkin takaisin lomakkeeseen.
+Käsittelijän tulee kirjautumisen onnistuessa käynnistää sessio. Tutustukaa siis uuteen asiaan $_SESSION.
+Session pitää muistaa kirjautumisenne ja tarjota kirjautuneelle logout-linkki.
 -->
 <?php
-  session_start();
-  $auth = array(
-    // PW SECURITY THRU THE R00F
-    "Timo" => "Soini"
-  );
-  $user = strip_tags($_POST['username']);
-  $pass = strip_tags($_POST['password']);
+session_start();
+$auth = array(
+  // PW SECURITY THRU THE R00F
+  "Timo" => "Soini"
+);
+$user = strip_tags($_POST['username']);
+$pass = strip_tags($_POST['password']);
 
-  if(!$_POST['username'] == "" && !$_POST['password'] == ""){
+if(!$_POST['username'] == "" && !$_POST['password'] == ""){
 
-    if ($auth["{$user}"] == $pass)
-    {
-        $_SESSION["logged_user"] = $user;
-        unset($_SESSION['error']);
-        header("location: success.php");
-    }
-    else {
-		header("location: index.php");
-      $_SESSION['error'] = "Wrong password!";
-    }
-  }
-  else
+  if ($auth[$user] == $pass)
   {
-	  header("location: index.php");
-	  $_SESSION['error'] = "You must fill both fields!";
+    $_SESSION["logged_user"] = $user;
+    unset($_SESSION['error']);
+    header("location: success.php");
   }
+  else {
+    header("location: index.php");
+    $_SESSION['error'] = "Wrong password!";
+  }
+}
+else
+{
+  header("location: index.php");
+  $_SESSION['error'] = "You must fill both fields!";
+}
 ?>
